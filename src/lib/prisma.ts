c0cedad0c81b0872847f8e40d2e079700554d5c7
AndrewSochi <virtual@playopt.ru>
2026-04-03 20:02:58 +0300

    fix: correct PrismaClientOptions for programmatic url

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,11 @@ let prisma: PrismaClient | undefined;
 export function getPrisma() {
   if (!prisma) {
     prisma = new PrismaClient({
-      datasourceUrl: process.env.DATABASE_URL,
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
     });
   }
   return prisma;
